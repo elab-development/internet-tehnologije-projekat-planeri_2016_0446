672fa5c0e1945aeac9446a068e15760b5c8ea572
Angelina Puzovic <baratovicjovana@gmail.com>
2024-04-08 21:45:04 +0200

    Model 'Role' dodat, migracija za 'Role' i povezivanje 'Role' sa 'User' modelom

--- planeri-backend/app/Models/User.php
+++ planeri-backend/app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
     /**
      * The attributes that are mass assignable.
      *
