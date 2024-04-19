7f25326c91fade1b4c8b7a8b71dc87810d0a240c
Angelina Puzovic <baratovicjovana@gmail.com>
2024-04-19 23:20:51 +0200

    Instaliran paket 'react-router-dom' i postavljen 'Router'

--- planeri-frontend/src/App.js
+++ planeri-frontend/src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./parts/Header";
+import { Route, Routes } from "react-router";
+import Home from "./parts/Home";
 
 function App() {
   return (
     <div className="App">
       <div className="flex flex-col w-full h-full gap-y-3 items-center">
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </div>
   );
