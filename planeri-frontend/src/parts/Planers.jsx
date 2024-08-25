db7dec65332646b1aa8429aec73c00e586064a8f
Angelina Puzovic <angelinapuzovic97@gmail.com>
2024-08-25 13:27:04 +0200

    Sortiranje planera po ceni

--- planeri-frontend/src/parts/Planers.jsx
+++ planeri-frontend/src/parts/Planers.jsx
@@ -3,12 +3,22 @@ import { useProductsService } from "../service/useProductsService";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PlanerCard from "../components/reusable/PlanerCard";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
 export default function Planers({ products, setProducts, setCartItems }) {
+  const [isAscending, setIsAscending] = useState(false);
   const { getProductsRequest } = useProductsService();
 
   const getProductsData = async () => {
     await getProductsRequest().then((result) => setProducts(result));
+  };
+
+  const sortProducts = () => {
+    const sortedProducts = isAscending
+      ? products.sort((a, b) => parseFloat(a.price) - parseFloat(b.price))
+      : products.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+    setProducts(sortedProducts);
+    setIsAscending(!isAscending);
   };
 
   const addToCart = (product) => {
@@ -34,6 +44,12 @@ export default function Planers({ products, setProducts, setCartItems }) {
   }, []);
   return (
     <div className="flex flex-col h-[560px] w-full overflow-y-auto px-20">
+      <div className="flex flex-row w-full justify-end items-center gap-3">
+        <p>Sortiraj po ceni:</p>
+        <div onClick={() => sortProducts()}>
+          {isAscending ? <FaArrowUp /> : <FaArrowDown />}
+        </div>
+      </div>
       <div className="flex flex-wrap w-full h-full justify-center items-center  gap-x-2 gap-y-2">
         {products?.map((prod) => (
           <PlanerCard
